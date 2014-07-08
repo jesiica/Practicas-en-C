7d73b6d289a144a0856f1741f8e4c81a850f0d9f
jesiica <jeszkita@gmail.com>
2014-07-08 13:21:47 -0500

    Suma valores desde teclado suma2.c

---
+++ suma2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{ 	int a, b, c;
+
+	
+	printf("valor de A:  ");
+	scanf("%d", &a);
+	printf("valor de B:  ");
+	scanf("%d", &b);
+
+	c=a+b;
+	printf("La suma es: %d \n", c );
+	
+		/* code */
+	return 0;
+} 
